e550b6c25fa8947a063147e009cb942574e4a259
Eric Jackson <ejackson@ashevillenc.gov>
2021-07-26 11:37:44 -0400

    Added remapping of API data into the right form

--- lambda_function.py
+++ lambda_function.py
@@ -6,21 +6,46 @@ def getProjects():
     print(token)
     client = asana.Client.access_token(token)
 
-    result = client.tasks.get_tasks(fields = ['gid', 'followers', 'assignee.name', 'name', 'custom_fields'], opt_pretty=True, project=1200128036189056)
+    result = client.tasks.get_tasks(fields = ['gid', 'followers', 'assignee.name', 'name', 'notes', 'custom_fields', 'completed_at', 'memberships.section.name'], opt_pretty=True, project=1200128036189056)
 
     #print(result)
     projects = []
     for item in result:
-        print(item)
-        projects.append(item)
+        project = {}
+        project['Name'] = item['name']
+        project['Notes'] = item['notes']
+        project['Completed At'] = item['completed_at']
+        project['Assignee'] = None
+        if item['assignee'] != None:
+            project['Assignee'] = item['assignee']['name']
+        project['Section/Column'] = None
+        if len(item['memberships']) > 0:
+            project['Section/Column'] = item['memberships'][0]['section']['name']
+        project['Importance'] = None
+        project['Status'] = None
+        if len(item['custom_fields']) > 0:
+            for fld in item['custom_fields']:
+                if fld['name'] == 'Importance':
+                    project['Importance'] = fld['display_value']
+                if fld['name'] == 'Status':
+                    project['Status'] = fld['display_value']
+        projects.append(project)
+       # projects.append(item)
     return projects
 
 def lambda_handler (event, context):
-    result = getProjects()
+    projects = getProjects()
+    print(len(projects))
+    count = 0
+    for project in projects:
+       # print(json.dumps(project))
+        count = count + 1
+        if count > 20:
+            break
     return {
         'statusCode': 200,
-        'body': result
+        'body': projects
     }
 
 ## For local testing
-#lambda_handler(None, None)
+lambda_handler(None, None)
--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import asana
+import os
 
-# We originally entered the Asana token here directly, but that's a terrible
-# idea because it then gets checked into Github and becomes vulnerable.
-client = asana.Client.access_token('ASANA_TOKEN_BAD_BAD_IDEA')
+# We originally entered the Asana token directly in the client call, but that's a terrible
+# idea because it then gets checked into Github and becomes vulnerable. Instead we will 
+# take it from the environment. 
+# 
+# To set the environment variable in Windows, enter the following:
+#
+#   set ASANA_TOKEN=token-string
+#
+# (replacing 'token-string' with the actual token)
+
+token = os.getenv('ASANA_TOKEN')
+client = asana.Client.access_token(token)
 
 result = client.tasks.get_tasks(fields = ['gid', 'followers', 'assignee.name', 'name', 'custom_fields'], opt_pretty=True, project=1200128036189056)
 
